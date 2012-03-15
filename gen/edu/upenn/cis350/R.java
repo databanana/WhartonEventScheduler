50bd7fff887c3592cb85f98384ba9a2cfc3dd342
Willy Huang <pommalicious@live.com>
2012-03-15 15:29:58 -0400

    Revert "Revert "Rough title bar""
    This reverts commit 94d7ef7b8d17c8d93275b4351760f39b83e757d5.

--- gen/edu/upenn/cis350/R.java
+++ gen/edu/upenn/cis350/R.java
@@ -11,20 +11,27 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int star_selector=0x7f020001;
+        public static final int calendar=0x7f020000;
+        public static final int control_button=0x7f020001;
+        public static final int gradient_background=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int left_right_borders=0x7f020004;
+        public static final int star_selector=0x7f020005;
     }
     public static final class id {
-        public static final int button1=0x7f050006;
-        public static final int button2=0x7f050007;
-        public static final int editText1=0x7f050009;
-        public static final int eventListOptions=0x7f050005;
-        public static final int eventListView=0x7f050004;
+        public static final int button1=0x7f050009;
+        public static final int button2=0x7f05000a;
+        public static final int calendar=0x7f050006;
+        public static final int editText1=0x7f05000c;
+        public static final int eventListOptions=0x7f050008;
+        public static final int eventListView=0x7f050007;
         public static final int event_name=0x7f050002;
         public static final int event_select_button=0x7f050000;
         public static final int event_time=0x7f050001;
+        public static final int event_title=0x7f050005;
         public static final int imageView1=0x7f050003;
-        public static final int linearLayout1=0x7f050008;
+        public static final int linearLayout1=0x7f05000b;
+        public static final int titleBar=0x7f050004;
     }
     public static final class layout {
         public static final int list_item=0x7f030000;
@@ -34,6 +41,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int event_name=0x7f040002;
         public static final int hello=0x7f040000;
     }
 }
