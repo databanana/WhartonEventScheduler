fb47e24be4c411f367513f8579835e9dcc428477
Travis Gingerich <gingerich.travis@gmail.com>
2012-03-16 13:37:28 -0400

    Added previously untracked files...oops

---
+++ src/edu/upenn/cis350/EventListActivity.java
@@ -0,0 +1,96 @@
+package edu.upenn.cis350;
+
+import android.app.*;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.*;
+import android.widget.*;
+import android.util.Log;
+import android.view.*;
+
+public class EventListActivity extends Activity {
+
+	private SQLiteDatabase db;
+	private ListView lv;
+	private CustomCursorAdapter dbadapter;
+	private boolean favoriteview = false;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Create a SQLite database holding event info
+		CustomOpenHelper dbopenhelper = new CustomOpenHelper(this);
+		db = dbopenhelper.getWritableDatabase();
+		
+		setContentView(R.layout.event_list);
+
+		this.lv = (ListView) findViewById(R.id.eventListView);
+		// Subclass CursorAdapter to read data from the database into the
+		// ListView
+		Cursor c = db.query("event", new String[] { "_id", "isfavorite",
+				"name", "starttime", "endtime" }, null, null, null, null,
+				"starttime asc");
+		dbadapter = new CustomCursorAdapter(this, c, false);
+		lv.setAdapter(dbadapter);
+	}
+
+	public void eventButtonClicked(int event_id, boolean isfavorite) {
+		int dbvalue;
+		if (isfavorite)
+			dbvalue = 1;
+		else
+			dbvalue = 0;
+		ContentValues cv = new ContentValues();
+		cv.put("isfavorite", dbvalue);
+		// Update database contents
+		db.update("event", cv, "_id=" + event_id, null);
+		if (favoriteview && dbvalue == 0) {
+			Cursor c = db.query("event", new String[] { "_id", "isfavorite",
+					"name", "starttime", "endtime" }, "isfavorite = 1", null,
+					null, null, "starttime asc");
+			dbadapter.changeCursor(c);
+		}
+		// Do something else when the button is clicked?
+		// Toast.makeText(getApplicationContext(), "Button clicked: " + event_id
+		// + " Pressed: " + isfavorite, Toast.LENGTH_SHORT).show();
+	}
+	
+	public void refresh(View v) { Log.d("refresh", "Called refresh"); } 
+
+	public void eventTextClicked(int event_id) {
+		// Do something when the event is clicked
+		Toast.makeText(getApplicationContext(), "Event clicked: " + event_id,
+				Toast.LENGTH_SHORT).show();
+	}
+
+	public void onDestroy() {
+		db.close();
+	}
+
+	public void showAllEvents() {
+		Log.d("click", "Showing all events");
+		favoriteview = false;
+		Cursor c = db.query("event", new String[] { "_id", "isfavorite",
+				"name", "starttime", "endtime" }, null, null, null, null,
+				"starttime asc");
+		dbadapter.changeCursor(c);
+	}
+
+	public void showMyEvents() {
+		Log.d("click", "Showing my events");
+		favoriteview = true;
+		Cursor c = db.query("event", new String[] { "_id", "isfavorite",
+				"name", "starttime", "endtime" }, "isfavorite = 1", null, null,
+				null, "starttime asc");
+		dbadapter.changeCursor(c);
+	}
+	
+	public void toggleMyEvents(View v) {
+		favoriteview = !favoriteview;
+		if (favoriteview) showMyEvents();
+		else showAllEvents();
+	}
+}
---
+++ src/edu/upenn/cis350/TabChooser.java
@@ -0,0 +1,35 @@
+package edu.upenn.cis350;
+
+import android.app.*;
+import android.content.*;
+import android.os.*;
+import android.widget.*;
+import android.view.*;
+import android.content.res.Resources;
+
+
+public class TabChooser extends TabActivity {
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main);
+	    Resources res = getResources();
+	    TabHost tabHost = getTabHost();
+	    TabHost.TabSpec spec;
+	    Intent intent;
+	    
+	    intent = new Intent().setClass(this, EventListActivity.class);
+	    spec = tabHost.newTabSpec("events")
+	    		.setIndicator("Events", res.getDrawable(R.drawable.people_tab_image))
+	    		.setContent(intent);
+	    tabHost.addTab(spec);
+	    
+	    intent = new Intent().setClass(this, WhartonAndroidAppPeopleActivity.class);
+	    spec = tabHost.newTabSpec("people")
+	    		.setIndicator("People", res.getDrawable(R.drawable.people_tab_image))
+	    		.setContent(intent);
+	    tabHost.addTab(spec);
+	    
+	    tabHost.setCurrentTab(1);
+	}
+
+}
